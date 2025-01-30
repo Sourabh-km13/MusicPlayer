d030607f95c7ce61157298d2ef000b69eb0c0622
sourabh kumar <lordgk02@gmail.com>
2025-01-30 22:34:42 +0530

    bug fixes

--- src/Components/molecules/PlayerController/PlayerController.jsx
+++ src/Components/molecules/PlayerController/PlayerController.jsx
@@ -12,102 +12,98 @@ import { timeFormatter } from "../../../utils/timeformatter";
 import { useEffect } from "react";
 
 
-export default function PlayerController(
-    {
-        playerstate,
-        handleSongLoad,
-        soundref,
-        handleShuffle,
-        previousSong,
-        setPlayerstate,
-        nextSong,
-        handleSeek,
-        songs,
-    }   
-){
-    
-    
-    return(
-        <div 
-        className="text-white"
-        >
+export default function PlayerController({
+    playerstate,
+    handleSongLoad,
+    soundref,
+    handleShuffle,
+    previousSong,
+    setPlayerstate,
+    nextSong,
+    handleSeek,
+    songs,
+}) {
+    return (
+        <div className="text-white">
             <ReactHowler
-            ref={soundref}
-            src={songs[playerstate.index].url}
-            playing={playerstate.playing}
-            onLoad={handleSongLoad}
-            onEnd={nextSong}
+                ref={soundref}
+                src={songs[playerstate.index].url}
+                playing={playerstate.playing}
+                onLoad={handleSongLoad}
+                onEnd={nextSong} // Automatically play the next song
             />
-            <div
-            className="flex justify-center items-center space-x-4 py-2 px-16 text-gray-400 w-fit"
-            >
+            <div className="flex justify-center items-center space-x-4 py-2 px-16 text-gray-400 w-fit">
                 <button
-                className={playerstate.shuffle?"text-2xl text-white":"text-2xl"}
-                onClick={handleShuffle}
+                    className={playerstate.shuffle ? "text-2xl text-white" : "text-2xl"}
+                    onClick={handleShuffle}
                 >
-                    <MdShuffle/>
+                    <MdShuffle />
                 </button>
-                <button
-                className="text-3xl"
-                onClick={previousSong}
-                >
-                    <MdSkipPrevious/>
+                <button className="text-3xl" onClick={previousSong}>
+                    <MdSkipPrevious />
                 </button>
-
-
-                {
-                        playerstate.playing?
-                        <button className="text-4xl text-white"
-                        onClick={()=>setPlayerstate((prevstate)=>({...prevstate,playing:false}))}
-                        >
-                        <MdOutlinePauseCircleFilled/>
-                        </button>:
-                        <button className="text-4xl text-white"
-                        onClick={()=>setPlayerstate((prevstate)=>({...prevstate,playing:true}))}
-                        >
-                            <MdPlayCircleFilled/>
-
-                        </button>
-                    }
-                <button
-                className="text-3xl"
-                onClick={nextSong}
-                >
-                    <MdSkipNext/>
+                {playerstate.playing ? (
+                    <button
+                        className="text-4xl text-white"
+                        onClick={() =>
+                            setPlayerstate((prevState) => ({
+                                ...prevState,
+                                playing: false,
+                            }))
+                        }
+                    >
+                        <MdOutlinePauseCircleFilled />
+                    </button>
+                ) : (
+                    <button
+                        className="text-4xl text-white"
+                        onClick={() =>
+                            setTimeout(() => {
+                                setPlayerstate((prevState) => ({
+                                    ...prevState,
+                                    playing: true,
+                                }))
+                            }, 0)
+                        }
+                    >
+                        <MdPlayCircleFilled />
+                    </button>
+                )}
+                <button className="text-3xl" onClick={nextSong}>
+                    <MdSkipNext />
                 </button>
-                <button
-                className="text-2xl"
-                >
-                    <MdOutlineRepeat/>
+                <button className="text-2xl">
+                    <MdOutlineRepeat />
                 </button>
-
             </div>
-            {/*progressbarr */}
+            {/* Progress bar */}
             <div className="flex items-center justify-center space-x-2 text-xs text-gray-400 py-2">
-                    <div className="text-left">
-                        {timeFormatter(playerstate.seek)}
-                    </div>
-                    <div className="w-full ">
-                        <input 
+                <div className="text-left">{timeFormatter(playerstate.seek)}</div>
+                <div className="w-full">
+                    <input
                         type="range"
-                        value={playerstate.seek} 
-                        className=" w-full bg-gray-800 accent-violet-600"
+                        value={playerstate.seek}
+                        className="w-full bg-gray-800 accent-violet-600"
                         min={0}
-                        max={playerstate.duration||0}
-                        step={0.1} 
-                        
+                        max={playerstate.duration || 0}
+                        step={0.5}
                         onChange={handleSeek}
-                        onMouseDown={()=>{setPlayerstate({...playerstate,isSeeking:true})}}
-                        onMouseUp={()=>{setPlayerstate({...playerstate,isSeeking:false})}}
-                        />
-                    </div>
-                    <div>
-                        {timeFormatter(playerstate.duration) }
-                    </div>
+                        onMouseDown={() =>
+                            setPlayerstate((prevState) => ({
+                                ...prevState,
+                                isSeeking: true,
+                            }))
+                        }
+                        onMouseUp={() =>
+                            setPlayerstate((prevState) => ({
+                                ...prevState,
+                                isSeeking: false,
+                            }))
+                        }
+                    />
+                </div>
+                <div>{timeFormatter(playerstate.duration)}</div>
             </div>
-            
         </div>
-    )
-
-    
+    );
 }
--- src/Components/molecules/PlayerController/PlayerControllerContainer.jsx
+++ src/Components/molecules/PlayerController/PlayerControllerContainer.jsx
@@ -36,122 +36,108 @@ export default function PlayerControllerContainer() {
                 url:Audio4
             }
           ]
-        const soundref = useRef(null)
-    
-        const [playerstate, setPlayerstate] = useState({
-            playing:false,
-            index:0,
-            seek:0.0,
-            isSeeking:false,
-            repeat:false,
-            shuffle:false,
-            duration:0.0,
-        })
-    
-        function nextSong() {
-            console.log("clicked next song");
-        
-            if (playerstate.shuffle) {
-                const randomIndex = Math.floor(Math.random() * songs.length);
-                setPlayerstate((prevState) => ({
-                    ...prevState,
-                    index: (randomIndex === prevState.index) ? (randomIndex + 1) % songs.length : randomIndex,
-                }));
-            } else {
-                setPlayerstate((prevState) => ({
-                    ...prevState,
-                    index: (prevState.index + 1) % songs.length,
-                }));
-            }
-        }
-        
-        function previousSong() {
-            console.log("previous song func called");
-        
-            setPlayerstate((prevState) => ({
-                ...prevState,
-                index: (prevState.index - 1 + songs.length) % songs.length,
-            }));
-        }
-        
-        function handleShuffle() {
-            console.log("shuffle called");
-        
-            setPlayerstate((prevState) => ({
-                ...prevState,
-                shuffle: !prevState.shuffle,
-            }));
-        }
-        
-        function handleSongLoad() {
-            if (soundref.current) {
-                setPlayerstate((prevState) => ({
-                    ...prevState,
-                    duration: soundref.current.duration(),
-                    seek:0
-                }));
-                
-            }
-        }
-        
-        
-        function handleSeek(e) {
-                     
-            const value = parseFloat(e.target.value);
-            if (soundref.current) {
-            soundref.current.seek(value);
-            }
-            setPlayerstate((prevState) => ({
-            ...prevState,
-            seek: value,
-            }));
-        }
-        
-        // Isolate `seek` updates to prevent interference
-        // useEffect(() => {
-        //     let id;
-        //     if (playerstate.playing && !playerstate.isSeeking) {
-        //         const updateSeek = () => {
-                                     
-        //             setPlayerstate((prevState)=>( {
-        //                 ...prevState,
-        //                 seek: soundref.current?.seek()
-        //             }));
-        //             id = requestAnimationFrame(updateSeek);
-        //         };
-        //         id = requestAnimationFrame(updateSeek);
-        //         return () => cancelAnimationFrame(id);
-        //     }
-        //     cancelAnimationFrame(id);
-        // }, [playerstate.playing, playerstate.isSeeking]);
-        
-        useEffect(() => {
+          const soundref = useRef(null);
+
+          const [playerstate, setPlayerstate] = useState({
+              playing: false,
+              index: 0,
+              seek: 0.0,
+              isSeeking: false,
+              repeat: false,
+              shuffle: false,
+              duration: 0.0,
+          });
+          
+          function nextSong() {
+              console.log("clicked next song");
+              if (playerstate.shuffle) {
+                  const randomIndex = Math.floor(Math.random() * songs.length);
+                  setPlayerstate((prevState) => ({
+                      ...prevState,
+                      index: randomIndex === prevState.index ? (randomIndex + 1) % songs.length : randomIndex,
+                      seek: 0,
+                  }));
+              } else {
+                  setPlayerstate((prevState) => ({
+                      ...prevState,
+                      index: (prevState.index + 1) % songs.length,
+                      seek: 0,
+                  }));
+              }
+          }
+          
+          function previousSong() {
+              console.log("previous song func called");
+              setPlayerstate((prevState) => ({
+                  ...prevState,
+                  index: (prevState.index - 1 + songs.length) % songs.length,
+                  seek: 0,
+              }));
+          }
+          
+          function handleShuffle() {
+              console.log("shuffle called");
+              setPlayerstate((prevState) => ({
+                  ...prevState,
+                  shuffle: !prevState.shuffle,
+              }));
+          }
+          
+          function handleSongLoad() {
+              if (soundref.current) {                  
+                  setPlayerstate((prevState) => ({
+                      ...prevState,
+                      duration: soundref.current.duration(),
+                      seek: 0,
+                  }));
+              }
+          }
+          
+          function handleSeek(e) {
+         
+            
+              const value = parseFloat(e.target.value);
+              if (soundref.current) {
+                  soundref.current.seek(value);
+              }
+              setPlayerstate((prevState) => ({
+                  ...prevState,
+                  seek: value,
+              }));
+          }
+          
+          // Synchronize `seek` with the current playback position
+          useEffect(() => {
+            let animationFrameId;
+
             if (playerstate.playing && !playerstate.isSeeking) {
                 const updateSeek = () => {
                     setPlayerstate((prevState) => ({
                         ...prevState,
                         seek: soundref.current?.seek() || 0,
                     }));
-                    requestAnimationFrame(updateSeek);
+                    animationFrameId = requestAnimationFrame(updateSeek);
                 };
-                const id = requestAnimationFrame(updateSeek);
-                return () => cancelAnimationFrame(id);
+                animationFrameId = requestAnimationFrame(updateSeek);
             }
-        }, [playerstate.playing, playerstate.isSeeking]);
         
-       
-        return (
-            <PlayerController
-                playerstate={playerstate}
-                setPlayerstate={setPlayerstate}
-                handleSeek={handleSeek}
-                handleShuffle={handleShuffle}
-                handleSongLoad={handleSongLoad}
-                previousSong={previousSong}
-                nextSong={nextSong}
-                soundref={soundref}
-                songs={songs}
-            />
-        );
+            return () => cancelAnimationFrame(animationFrameId);
+          }, [playerstate.playing, playerstate.isSeeking]);
+
+  
+          return (
+              <PlayerController
+                  playerstate={playerstate}
+                  setPlayerstate={setPlayerstate}
+                  handleSeek={handleSeek}
+                  handleShuffle={handleShuffle}
+                  handleSongLoad={handleSongLoad}
+                  previousSong={previousSong}
+                  nextSong={nextSong}
+                  soundref={soundref}
+                  songs={songs}
+              />
+          );
+          
         
 }
